1c13b0a2040ce8828a6d58fe14b72e1cf1e13f5d
milaejimenez <milaejimenez@gmail.com>
2022-12-28 22:45:01 +0100

    Fetch products data from api and display it on the page

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,7 +1,35 @@
 import './Products.css';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getStaticContextFromError } from '@remix-run/router';
 
 export default function Products() {
-  return <div>Products</div>;
+  const [products, setProducts] = useState(null);
+  const [error, setError] = useState(null);
+
+  //Fetch data
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => {
+        setError('Could not fetch the data');
+        console.log(error);
+      });
+  }, [setProducts, setError]);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      {products &&
+        products.map((product) => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <img src={product.image} alt={product.title} />
+            <p>{product.description}</p>
+          </div>
+        ))}
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
