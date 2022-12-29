6c668de9c043d8549bb8db4a0ad46fb19275a0f2
milaejimenez <milaejimenez@gmail.com>
2022-12-29 09:50:03 +0100

    Add buttons to select products by category
    Each button fires an onClick that changes the fetched url to the chosen category

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,26 +1,76 @@
 import './Products.css';
 
 import React, { useEffect, useState } from 'react';
-import { getStaticContextFromError } from '@remix-run/router';
+
+//Url paths
+const allProducts = 'https://fakestoreapi.com/products';
+const selectCategories = 'https://fakestoreapi.com/products/category';
+const electronics = selectCategories + '/electronics';
+const jewelery = selectCategories + '/jewelery';
+const womensClothing = selectCategories + "/women's%20clothing";
+const mensClothing = selectCategories + "/men's%20clothing";
 
 export default function Products() {
   const [products, setProducts] = useState(null);
   const [error, setError] = useState(null);
+  const [url, setUrl] = useState(allProducts);
 
   //Fetch data
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/category/women's%20clothing")
+    fetch(url)
       .then((res) => res.json())
       .then((data) => setProducts(data))
       .catch((error) => {
         setError('Could not fetch the data');
         console.log(error);
       });
-  }, [setProducts, setError]);
+  }, [setProducts, setError, url]);
+
+  //Set the url
+  const handleUrl = function (path) {
+    setUrl(path);
+  };
 
   return (
     <div>
       <h1>Products</h1>
+      <div>
+        <button type="button" onClick={() => handleUrl(allProducts)}>
+          All Products
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleUrl(electronics);
+          }}
+        >
+          Electronics
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleUrl(jewelery);
+          }}
+        >
+          Jewelery
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleUrl(womensClothing);
+          }}
+        >
+          Women's Clothing
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleUrl(mensClothing);
+          }}
+        >
+          Men's Clothing
+        </button>
+      </div>
       {products &&
         products.map((product) => (
           <div key={product.id}>
